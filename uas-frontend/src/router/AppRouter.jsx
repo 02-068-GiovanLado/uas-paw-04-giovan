c5772f37b47fdb14f780abbda758d7b756ab0e08
01-151-HildyahMaretasya <hildyah.123140151@student.itera.ac.id>
2025-12-09 19:22:59 +0700

    memperbaiki router untuk chat agent

--- uas-frontend/src/router/AppRouter.jsx
+++ uas-frontend/src/router/AppRouter.jsx
@@ -1,39 +1,47 @@
 import { Routes, Route } from "react-router-dom";
 
-// Pages
+// Public Pages
 import Home from "../pages/Home";
 import Listing from "../pages/Listing";
 import Detail from "../pages/Detail";
+import Favorite from "../pages/Favorite";
+
+// Auth
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import Favorite from "../pages/Favorite";
+
+// Agent Pages
 import AgentProfile from "../pages/AgentProfile";
-import Chat from "../pages/Chat";
 import AgentDashboard from "../pages/AgentDashboard";
 import AddProperty from "../pages/AddProperty";
 import EditProperty from "../pages/EditProperty";
 
+// Chat
+import Chat from "../pages/Chat";
+
 export default function AppRouter() {
   return (
     <Routes>
+
       {/* PUBLIC PAGES */}
       <Route path="/" element={<Home />} />
       <Route path="/listing" element={<Listing />} />
       <Route path="/property/:id" element={<Detail />} />
       <Route path="/favorite" element={<Favorite />} />
 
-      {/* AUTH PAGES */}
+      {/* AUTH */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
 
-      {/* AGENT PAGES */}
-      <Route path="/agent/:id" element={<AgentProfile />} />
-      <Route path="/chat/:id" element={<Chat />} />
+      {/* AGENT ROUTES */}
+      <Route path="/agent/profile/:id" element={<AgentProfile />} />
+      <Route path="/agent/dashboard" element={<AgentDashboard />} />
+      <Route path="/agent/add-property" element={<AddProperty />} />
+      <Route path="/agent/edit-property/:id" element={<EditProperty />} />
 
-      {/* DASHBOARD & PROPERTY MANAGEMENT */}
-      <Route path="/dashboard" element={<AgentDashboard />} />
-      <Route path="/add-property" element={<AddProperty />} />
-      <Route path="/edit-property/:id" element={<EditProperty />} />
+      {/* CHAT SYSTEM */}
+      <Route path="/chat/:agentId" element={<Chat />} />
+
     </Routes>
   );
 }
